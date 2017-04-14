d4f29c0aa7b7a412bb7d7609e387631db407fc99
Benoit <benoitcsirois@gmail.com>
2017-04-14 13:16:59 -0400

    Removed ubuntu-specific packages and added dependencies to work with GTK3, extracted from https://github.com/ubuntu/snapcraft-desktop-helpers/

--- scripts/emacsclient-tealeg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-EMACSVERSION=25.1
-export EMACSDATA=$SNAP/share/emacs/$EMACSVERSION/etc
-export EMACSLOADPATH=$SNAP/share/emacs/site-lisp:$SNAP/share/emacs/$EMACSVERSION/lisp/
-export INFOPATH=$SNAP/share/info/
-desktop-launch emacsclient "$@"
